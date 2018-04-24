be182037d24c2520ec7e5861aa1de707543e14ed
Andrii Dziuba <andrii.dziuba.dux@gmail.com>
2018-04-24 07:54:07 +0300

    Init

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase:{
+    apiKey: "AIzaSyCGoDuNe8J_4Ur5-HrmZv0MYzre7dsL2oY",
+    authDomain: "oshop-8a438.firebaseapp.com",
+    databaseURL: "https://oshop-8a438.firebaseio.com",
+    projectId: "oshop-8a438",
+    storageBucket: "oshop-8a438.appspot.com",
+    messagingSenderId: "414631532203"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,13 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase:{
+    apiKey: "AIzaSyCGoDuNe8J_4Ur5-HrmZv0MYzre7dsL2oY",
+    authDomain: "oshop-8a438.firebaseapp.com",
+    databaseURL: "https://oshop-8a438.firebaseio.com",
+    projectId: "oshop-8a438",
+    storageBucket: "oshop-8a438.appspot.com",
+    messagingSenderId: "414631532203"
+  }
 };
